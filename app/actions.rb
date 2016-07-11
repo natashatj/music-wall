aa72d197541c3159a949547e5a1d733b8ddcf285
Natasha Jadavji <natasha@jadavji.ca>
2016-07-11 19:26:12 +0000

    app basics

--- app/actions.rb
+++ app/actions.rb
@@ -11,3 +11,22 @@ end
 get '/tracks/new' do
   erb :'tracks/new'
 end
+
+get '/tracks/:id' do
+  @track = Track.find params[:id]
+  erb :'tracks/show'
+end
+
+post '/tracks' do
+  @track = Track.new(
+    title: params[:title],
+    artist: params[:artist]
+    )
+  @track.save
+  redirect '/tracks'
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new'
+  end
+end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,4 @@
 class Track < ActiveRecord::Base
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :artist, presence: true, length: { maximum: 40 }
 end
